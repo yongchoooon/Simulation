fecbe405a10b9e15feeb1c8965b71a2554cd1f7b
yongchoooon <sod7050@gmail.com>
2022-11-28 21:21:55 +0900

    에이전트, 장애물 생성 완료

--- MoneyModel.py
+++ MoneyModel.py
@@ -4,6 +4,32 @@ from mesa.space import MultiGrid
 from mesa.datacollection import DataCollector
 from mesa.batchrunner import BatchRunner
 
+inven_coords = [(4,12), (5,12), (6,12), (7,12), (10,12), (11,12), (12,12), (13,12),
+                (4,11), (5,11), (6,11), (7,11), (10,11), (11,11), (12,11), (13,11),
+                (4,8), (5,8), (6,8), (7,8), (10,8), (11,8), (12,8), (13,8),
+                (4,7), (5,7), (6,7), (7,7), (10,7), (11,7), (12,7), (13,7),
+                (4,4), (5,4), (6,4), (7,4), (10,4), (11,4), (12,4), (13,4),
+                (4,3), (5,3), (6,3), (7,3), (10,3), (11,3), (12,3), (13,3)]
+
+parking_lots = [(3,12),(14,12),
+                (3,11),(14,11),
+                (3,8),(14,8),
+                (3,7),(14,7),
+                (3,4),(14,4),
+                (3,3),(14,3)]
+
+conveyors = [(0,15),(1,15),(2,15),(3,15),(4,15),(5,15),(6,15),(7,15),(8,15),(9,15),(10,15),(11,15),(12,15),(13,15),(14,15),(15,15),(16,15),
+             (0,14),(0,13),(0,12),(0,11),(0,10),(0,9),(0,8),(0,7),(0,6),(0,5),(0,4),(0,3),(0,2),(0,1),
+             (0,0),(1,0),(2,0),(3,0),(4,0),(5,0),(6,0),(7,0),(8,0),(9,0),(10,0),(11,0),(12,0),(13,0),(14,0),(15,0),(16,0)]
+
+obstacles = inven_coords + conveyors
+
+all = []
+for x in range(17):
+    for y in range(16):
+        all.append((x, y))
+
+worker_spaces = list(set(all) - set(obstacles))
 
 def compute_gini(model):
     # Do math here!
@@ -19,6 +45,7 @@ class MoneyAgent(Agent):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
         self.wealth = 1
+        self.name = "Worker"
 
     def step(self):
         self.move()
@@ -29,34 +56,79 @@ class MoneyAgent(Agent):
         possible_steps = self.model.grid.get_neighborhood(
           self.pos,
           moore=False,
-          include_center=True)
+          include_center=False)
+        possible_steps = list(set(possible_steps) - set(obstacles))
+        print(possible_steps)
         new_position = self.random.choice(possible_steps)
+        # print(possible_steps)
         self.model.grid.move_agent(self, new_position)
 
     def give_money(self):
         cellmates = self.model.grid.get_cell_list_contents([self.pos])
         if len(cellmates) > 1:
+            # print(cellmates[0].pos)
             other = self.random.choice(cellmates)
             other.wealth += 1
             self.wealth -= 1
 
+class Inventory(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.name = "Inventory"
+        self.wealth = -99
+    
+class ParkingLot(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.name = "ParkingLot"
+        self.wealth = -99
+
+class Conveyor(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.name = "Conveyor"
+        self.wealth = -99
 
 class MoneyModel(Model):
     """Our model--a home for our agents :)"""
     def __init__(self, N, width, height):
         self.num_agents = N
-        self.grid = MultiGrid(width, height, True)
         # A physical world to place our agents in 
         self.grid = MultiGrid(width, height, True)
         self.schedule = RandomActivation(self)
         self.running = True
+
         # Create agents
         for i in range(self.num_agents):
-            a = MoneyAgent(i, self)
+            no = i
+            a = MoneyAgent(no, self)
             self.schedule.add(a)
-            x = self.random.randrange(self.grid.width)
-            y = self.random.randrange(self.grid.height)
+            x, y = a.random.choice(worker_spaces)
             self.grid.place_agent(a, (x, y))
+
+        # Create inventorys
+        for j in range(len(inven_coords)):
+            no = j + self.num_agents
+            iv = Inventory(no, self)
+            self.schedule.add(iv)
+            x, y = inven_coords[j]
+            self.grid.place_agent(iv, (x, y))
+
+        # Create parking lots
+        for k in range(len(parking_lots)):
+            no = k + no + 1
+            pl = ParkingLot(no, self)
+            self.schedule.add(pl)
+            x, y = parking_lots[k]
+            self.grid.place_agent(pl, (x, y))
+
+        # Create conveyors
+        for l in range(len(conveyors)):
+            no = l + no + 1
+            cv = Conveyor(no, self)
+            self.schedule.add(cv)
+            x, y = conveyors[l]
+            self.grid.place_agent(cv, (x, y))
 
         # Some metrics we'll measure about our model
         self.datacollector = DataCollector(
--- main.py
+++ main.py
@@ -6,27 +6,39 @@ from MoneyModel import MoneyModel
 
 def agent_portrayal(agent):
     portrayal = {
-      "Shape": "circle",
+      "Shape": "rect",
       "Filled": "true",
-      "r": agent.wealth + 0.5}
-    if agent.wealth > 2:
-      portrayal["Color"] = "red"
+      "w": 1,
+      "h": 1}
+
+    if agent.name == "Worker":
+      if agent.wealth > 2:
+        portrayal["Color"] = "red"
+        portrayal["Layer"] = 1
+      elif agent.wealth > 1:
+        portrayal["Color"] = "orange"
+        portrayal["Layer"] = 2
+      elif agent.wealth > 0:
+        portrayal["Color"] = "navy"
+        portrayal["Layer"] = 3
+      else:
+        portrayal["Color"] = "pink"
+        portrayal["Layer"] = 4
+    elif agent.name == "Inventory":
+      portrayal["Color"] = "#b8c6e4"
       portrayal["Layer"] = 0
-    elif agent.wealth > 1:
-      portrayal["Color"] = "orange"
-      portrayal["Layer"] = 2
-    elif agent.wealth > 0:
-      portrayal["Color"] = "yellow"
-      portrayal["Layer"] = 3
-    else:
+    elif agent.name == "ParkingLot":
+      portrayal["Color"] = "#e4eedc"
+      portrayal["Layer"] = 0
+    elif agent.name == "Conveyor":
       portrayal["Color"] = "lightgrey"
-      portrayal["Layer"] = 4
+      portrayal["Layer"] = 0
     return portrayal
 
 grid = CanvasGrid(
   agent_portrayal,
-  30,
-  30,
+  17,
+  16,
   800,
   800)
 
@@ -39,7 +51,7 @@ server = ModularServer(
   MoneyModel,
   [grid, chart],
   "Money Model",
-  {"N":50, "width":30, "height":30}
+  {"N":6, "width":17, "height":16}
   )
-server.port = 8521 # Any non-80 port to appease replit
+server.port = 8523 # Any non-80 port to appease replit
 server.launch()
